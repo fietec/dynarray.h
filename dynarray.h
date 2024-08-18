ab19dfd3650fc152e56e4bd4c9dc693720e4d364
cdm <106453380+OverflowedFiete@users.noreply.github.com>
2024-08-18 02:00:07 +0200

    commited dynarray.h header file
    This is the initial commit of the program with its core functionality.

---
+++ dynarray.h
@@ -0,0 +1,73 @@
+#ifndef _DYNARRAY_H
+#define _DYNARRAY_H
+
+/*
+    dynarray.h by cdm ©August 2024
+
+    A simple header-only macro-based implementation of a dynamic array of any specific type.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEFINE_DYNAMIC_ARRAY(name, type)\
+    typedef void* (*p_func) (type); /*value printing function*/ \
+    typedef void* (*f_func) (type); /*value freeing function*/ \
+    typedef struct{\
+        type* items;\
+        size_t size;\
+        size_t capacity;\
+    } name##_t;\
+    name##_t* name##_create(size_t capacity){\
+        name##_t* d_array = (name##_t*) malloc(sizeof(name##_t));\
+        if (d_array){\
+            type* items = (type*) calloc(capacity, sizeof(type));\
+            if (!items){\
+                free(d_array);\
+                return NULL;\
+            }\
+            d_array->items = items;\
+            d_array->capacity = capacity;\
+            d_array->size = 0;\
+        }\
+        return d_array;\
+    }\
+    int name##_push(name##_t* d_array, type value){\
+        if (!d_array || !d_array->items) return 1;\
+        if (d_array->size >= d_array->capacity){\
+            size_t new_capacity = d_array->capacity*2;\
+            type* new_items = (type*) realloc(d_array->items, sizeof(type*)*new_capacity);\
+            if (!new_items) return 1;\
+            d_array->items = new_items;\
+            d_array->capacity = new_capacity;\
+        }\
+        d_array->items[d_array->size++] = value;\
+        return 0;\
+    }\
+    void name##_print(name##_t* d_array, p_func func){\
+        puts("[");\
+        for (int i=0; i<d_array->size; ++i){\
+            printf("    Index: % 3d : ", i);\
+            if (func){\
+                func(d_array->items[i]);\
+                putchar('\n');\
+            }\
+            else{\
+                printf("Value of type "#type"\n");\
+            }\
+        }\
+        puts("]\n");\
+    }\
+    void name##_free(name##_t* d_array, f_func func){\
+        if (d_array){\
+            if (d_array->items){\
+                for (int i=0; i<d_array->size; ++i){\
+                    if (func != NULL) {func(d_array->items[i]);}\
+                }\
+                free(d_array->items);\
+            }\
+            free(d_array);\
+        }\
+    }
+
+#endif // _DYNARRAY_H
